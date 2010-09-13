b297ff31e556cd5658bdb213de375f70fa7df90c
Peter Kröner <peter@peterkroener.de>
2010-09-13 16:31:10 +0200

    Communicate slide controls from the presenter to the presentation
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- assets/js/presenter.js
+++ assets/js/presenter.js
@@ -1,22 +1,31 @@
-// Get the iframes
-var current = document.getElementById('current');
-var next = document.getElementById('next');
-window.addEventListener('load', function(event){
+window.addEvent('load', function(event){
+
+
+	// Get the iframes
+	var current = $('current');
+	var next = $('next');
+
+
 	// Get the number of slides
-	document.getElementById('numslides').innerHTML = current.contentWindow.document.getElementsByClassName('slide').length;
+	$('numslides').set('html', current.contentWindow.$$('.slide').length);
+
+
 	// Get the presentation's title
-	document.getElementById('title').innerHTML = current.contentWindow.document.getElementsByTagName('title')[0].innerHTML;
-	// Hide the control elements in the iframe
-	current.contentWindow.document.getElementById('controls').innerHTML = '';
-	next.contentWindow.document.getElementById('controls').innerHTML = '';
+	$('title').set('html', current.contentWindow.$$('title')[0].get('html'));
+
+
 	// Start from where the presenter was launched
-	var fragment = /#([0-9]+)$/.exec(window.location)[1];
-	if(fragment){
-		current.contentWindow.slideTo(fragment);
-		next.contentWindow.slideTo(++fragment);
+	var fragment = parseInt(/#([0-9]+)$/.exec(window.location)[1]);
+	if(!fragment){
+		fragment = 0;
 	}
+	$('currentindex').set('html', fragment);
+	current.contentWindow.slideTo(fragment);
+	next.contentWindow.slideTo(++fragment);
+
+
 	// Setup the timer
-	var time = document.getElementById('time');
+	var time = $('time');
 	var h = m = s = 0;
 	var timer = setInterval(function(){
 		s++;
@@ -36,14 +45,30 @@ window.addEventListener('load', function(event){
 		if(s.length == 1) { s = '0' + s; }
 		time.innerHTML = h + ':' + m + ':' + s;
 	}, 1000);
-}, false);
-// On message, advance the iframes
-window.addEventListener('message', function(event){
-	var currentslide = parseInt(event.data);
-	var nextslide = parseInt(event.data) + 1;
-	current.contentWindow.slideTo(currentslide);
-	next.contentWindow.slideTo(nextslide);
-	document.getElementById('currentindex').innerHTML = nextslide;
-}, false);
-// On keypress, advance the slides in the presentation
+
+
+	// On message, advance the iframes
+	window.addEventListener('message', function(event){
+		var currentslide = parseInt(event.data);
+		var nextslide = parseInt(event.data) + 1;
+		current.contentWindow.slideTo(currentslide);
+		next.contentWindow.slideTo(nextslide);
+		$('currentindex').set('html', nextslide);
+	}, false);
+
+
+	// On keypress, advance the slides in the presentation
+	if(window.opener){
+		window.addEvents({
+			'slidenext': function(){
+				window.opener.slideNext(true);
+			},
+			'slideback': function(){
+				window.opener.slideBack(true);
+			}
+		});
+	}
+
+
+});
 
