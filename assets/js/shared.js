1ab2946c54e423e4effbc5f837c5d5e977a73155
Peter Kröner <peter@peterkroener.de>
2011-02-18 17:55:33 +0100

    Created a worker file, added init code to shared.js
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- assets/js/shared.js
+++ assets/js/shared.js
@@ -4,22 +4,31 @@
 */
 
 
+// PIK5 global object
+var _PIK5 = {};
+
+
+// Create the worker for sharing information between presentation and presenter
+if(typeof SharedWorker == 'function'){
+	_PIK5.worker = new SharedWorker('assets/js/worker.js', '_PIK5');
+	_PIK5.port = _PIK5.worker.port;
+	_PIK5.hasWorker = true;
+}
+
+
+// Catch keypress events
 jQuery(document).ready(function($){
-
-
-$(document).keydown(function(evt){
-	var code = evt.keyCode;
-	if(code == 39 || code == 34){
-		$(this).trigger('slidenext');
-	}
-	else if(code == 37 || code == 33){
-		$(this).trigger('slideback');
-	}
-	else if(code == 116 || code == 190 || code == 27){
-		$(this).trigger('overlay');
-		evt.preventDefault();
-	}
-});
-
-
+	$(document).keydown(function(evt){
+		var code = evt.keyCode;
+		if(code == 39 || code == 34){
+			$(this).trigger('slidenext');
+		}
+		else if(code == 37 || code == 33){
+			$(this).trigger('slideback');
+		}
+		else if(code == 116 || code == 190 || code == 27){
+			$(this).trigger('overlay');
+			evt.preventDefault();
+		}
+	});
 });
---
+++ assets/js/worker.js
@@ -0,0 +1,13 @@
+// Connection pool and slide number
+var pool = []
+  , slide = 0;
+
+
+// Connection logic
+self.onconnect = function(event){
+	var port = event.ports[0];
+	port.onmessage = function(msg){
+		
+	};
+};
+
